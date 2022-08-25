2a12e7824e2e8f90336a7d4039b03df3e216aa6b
Kang <canfrog@naver.com>
2022-08-25 23:52:54 +0900

    Little bit Edit Code, LinearQueue.c

--- 05_Queue/LinearQueue.c
+++ 05_Queue/LinearQueue.c
@@ -24,7 +24,7 @@ void init_queue(QueueType *q) {
 void queue_print(QueueType *q) {
     for (int i = 0; i < MAX_QUEUE_SIZE; i++) {
         if ( i <= q->front || i > q->rear)
-            printf(" | ");
+            printf("   | ");
         else
             printf("%d | ", q->data[i]);
     }
