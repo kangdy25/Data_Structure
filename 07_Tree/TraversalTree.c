5aaf1aa347d3a4b93b5c288203d45683a6e92a8d
kangdy25 <58673491+kangdy25@users.noreply.github.com>
2022-12-15 20:33:19 +0900

    Update TraversalTree.c

--- 07_Tree/TraversalTree.c
+++ 07_Tree/TraversalTree.c
@@ -19,21 +19,21 @@ TreeNode n5 = { 20, &n3, &n4 };
 TreeNode n6 = { 15, &n2, &n5 };
 TreeNode *root = &n6;
 
-// 중위 순회
-void inorder(TreeNode *root) {
-    if (root != NULL) {
-        inorder(root->left);            // 왼쪽 서브트리 순회
-        printf("[%d] ", root->data);    // 노드 방문
-        inorder(root->right);           // 오른쪽 서브트리 순회
-    }
-}
-
 // 전위 순회
 void preorder(TreeNode *root) {
     if (root != NULL) {
         printf("[%d] ", root->data);    // 노드 방문
         preorder(root->left);           // 왼쪽 서브트리 순회
         preorder(root->right);          // 오른쪽 서브트리 순회
+    }
+}
+
+// 중위 순회
+void inorder(TreeNode *root) {
+    if (root != NULL) {
+        inorder(root->left);            // 왼쪽 서브트리 순회
+        printf("[%d] ", root->data);    // 노드 방문
+        inorder(root->right);           // 오른쪽 서브트리 순회
     }
 }
 
@@ -47,15 +47,15 @@ void postorder(TreeNode *root) {
 }
 
 int main() {
-    printf("중위 순회 = ");
-    inorder(root);
-    printf("\n");
-
-    printf("중위 순회 = ");
+    printf("전위 순회 = ");
     preorder(root);
     printf("\n");
 
     printf("중위 순회 = ");
+    inorder(root);
+    printf("\n");
+
+    printf("후위 순회 = ");
     postorder(root);
     printf("\n");
 
